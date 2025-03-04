13b17677ee057909d53a45614b0f9c1d79f2779f
nur-sh <nsheikhmohammed01@gmail.com>
2025-03-03 22:28:59 -0500

    add click for popup icon

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const shareIcon = document.querySelector('.article-profile .share-icon');
-const articleShare = document.querySelector('.article-share');
+const popupShareIcon = document.querySelector('.popup-share-icon');
+const articleShare = document.querySelector('.article-share-popup');
 
 if (shareIcon) {
   shareIcon.addEventListener('click', () => {
-    if (articleShare && articleShare.style.display === 'none') {
-      articleShare.style.setProperty('display', 'flex');
-    } else {
-      articleShare.style.setProperty('display', 'none');
-    }
+    articleShare.style.setProperty('display', 'flex');
+  })
+}
+
+if (popupShareIcon) {
+  popupShareIcon.addEventListener('click', () => {
+    articleShare.style.setProperty('display', 'none');
   })
 }
